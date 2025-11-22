5adc81d420f0881e1d8539ded637b73979c66ace
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2025-11-22 15:28:07 +0100

    aggiornato sentry

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
+import sys
 from contextlib import asynccontextmanager
 
 import sentry_sdk
-import sys
+from sentry_sdk.integrations.httpx import HttpxIntegration
 from fastapi import FastAPI, APIRouter
 from fastapi.responses import ORJSONResponse
 
@@ -17,21 +18,33 @@ import_models()  # Importo i modelli perché siano disponibili per le relazioni 
 
 sentry_sdk.init(
     dsn=settings.SENTRY_DSN,
+    environment=settings.ENVIRONMENT,
     send_default_pii=True,
-    release=settings.SENTRY_RELEASE,
+    enable_tracing=True,
+    profile_session_sample_rate=1.0,
+    profile_lifecycle="trace",
+    profiles_sample_rate=1.0,
+    enable_logs=True,
+    integrations=[HttpxIntegration()],
+    release=settings.SENTRY_RELEASE
 )
 
+sentry_sdk.set_tag("service.name", settings.SERVICE_NAME)
 logger = None
+
 
 # RabbitMQ Broker
 async def callback(message):
     async with message.process():
-        print(f"Received message from exchange '{message.exchange}' with routing key '{message.routing_key}': {message.body.decode()}")
+        print(
+            f"Received message from exchange '{message.exchange}' with routing key '{message.routing_key}': {message.body.decode()}")
+
 
 exchanges = {
     "users": callback,
     "banana": callback
 }
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -42,7 +55,7 @@ async def lifespan(app: FastAPI):
     # Avvia il broker asincrono all'avvio dell'app
     broker_instance = broker.AsyncBrokerSingleton()
     connected = await broker_instance.connect()
-    if(not connected):
+    if (not connected):
         logger.error("Could not connect to RabbitMQ. Exiting...")
         sys.exit(1)
         return
@@ -57,6 +70,7 @@ async def lifespan(app: FastAPI):
     logger.info(f"Shutting down {settings.SERVICE_NAME}...")
     await broker_instance.close()
     logger.info("RabbitMQ connection closed.")
+
 
 docs_url = None if settings.ENVIRONMENT == "production" else "/docs"
 redoc_url = None if settings.ENVIRONMENT == "production" else "/redoc"
@@ -80,6 +94,7 @@ current_router.include_router(
 )
 
 app.include_router(current_router, prefix=settings.API_PREFIX)
+
 
 @app.get("/health", tags=["health"])
 def health():
